19a851848fc4304b32aeadddaf8939c64ae3e7c1
Annika Ehlers <e_hlers@live.dk>
2020-05-12 23:02:10 +0200

    Start flask app implementation

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # PythonExam Song Suggestions Project
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data/song_data.csv", sep=",", header=0)
+
+print(df.columns)
+
+print(df['audio_valence'])
