678f2f6d3e24c39c93871171369877552d4c442f
Annika Ehlers <e_hlers@live.dk>
2020-05-13 11:24:31 +0200

    Make dropdown with songs in dataset

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, Markup, request
+import pandas as pd
 
 app = Flask(__name__)
+
+df = pd.read_csv("data/song_data.csv")
+songs = df.iloc[:, 0]
 
 
 @app.route("/", methods=["GET", "POST"])
 def show_frontpage():
-    return render_template("index.html")
+    return render_template("index.html", songs=songs)
