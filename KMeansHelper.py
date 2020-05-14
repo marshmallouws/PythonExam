8d34d8f45a9cc7811a993b7582922776ec7ce60e
vl48 <wegokr@gmail.com>
2020-05-14 13:39:55 +0200

    minibatchmeans in class

--- KMeansHelper.py
+++ KMeansHelper.py
@@ -1,22 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import MeanShift
-from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
+
 
 class KMeansHelper:
 
     def __init__(self, pathtofile, clusters):
         self.fileToRead = pathtofile
         self.clusters = clusters
-        self.kmeansObj = KMeans(n_clusters=self.clusters)
+        self.kmeansObj = MiniBatchKMeans(n_clusters=self.clusters)
         self.fileData = pd.read_csv(self.fileToRead)
-        self.kmeansObj.fit(self.fileData.drop('song_name',axis=1))
+        self.kmeansObj.fit(self.fileData.drop(
+            ['song_name', 'song_popularity', 'song_duration_ms', 'key', 'loudness', 'audio_mode', 'tempo', 'time_signature'], axis=1))
 
     def getClusters(self):
         return self.kmeansObj.cluster_centers_
 
     def getLabels(self):
         return self.kmeansObj.labels_
-
-    
-    
