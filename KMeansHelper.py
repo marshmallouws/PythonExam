427902b3640363cb19d49cd1a2dc41a6bab6b19a
vl48 <wegokr@gmail.com>
2020-05-18 11:05:39 +0200

    kmhelper load model

--- KMeansHelper.py
+++ KMeansHelper.py
@@ -1,18 +1,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import pickle
 from sklearn.cluster import MeanShift
 from sklearn.cluster import MiniBatchKMeans
 
 
 class KMeansHelper:
+    MODEL_PATH = 'data/minibatchkmeans.model'
 
-    def __init__(self, pathtofile, clusters):
-        self.fileToRead = pathtofile
-        self.clusters = clusters
-        self.kmeansObj = MiniBatchKMeans(n_clusters=self.clusters)
-        self.fileData = pd.read_csv(self.fileToRead)
-        self.kmeansObj.fit(self.fileData.drop(
-            ['song_name', 'song_popularity', 'song_duration_ms', 'key', 'loudness', 'audio_mode', 'tempo', 'time_signature'], axis=1))
+    def __init__(self, pathtofile=None, clusters=None):
+        if pathtofile is None or clusters is None:
+            self.load_model()
+        else:
+            self.fileToRead = pathtofile
+            self.clusters = clusters
+            self.kmeansObj = MiniBatchKMeans(n_clusters=self.clusters)
+            self.fileData = pd.read_csv(self.fileToRead)
+            self.kmeansObj.fit(self.fileData.drop(
+                ['song_name', 'song_popularity', 'song_duration_ms', 'key', 'loudness', 'audio_mode', 'tempo', 'time_signature'], axis=1))
+
+    def load_model(self):
+        self.kmeansObj = pickle.load(open(self.MODEL_PATH, "rb"))
 
     def getClusters(self):
         return self.kmeansObj.cluster_centers_
@@ -20,5 +28,8 @@ class KMeansHelper:
     def getLabels(self):
         return self.kmeansObj.labels_
 
-    def getModel(self):
+    def get_model(self):
         return self.kmeansObj
+
+    def get_model_path(self):
+        return self.MODEL_PATH
--- save_model.py
+++ save_model.py
@@ -3,4 +3,4 @@ import KMeansHelper as kmh
 
 mbkm = kmh.KMeansHelper(pathtofile="data/song_data.csv", clusters=100)
 
-pickle.dump(mbkm.getModel(), open('data/minibatchkmeans.model', 'wb'))
+pickle.dump(mbkm.get_model(), open(mbkm.get_model_path(), 'wb'))
