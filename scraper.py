acb012d9cefb8d050721061590ff58bf97d7cfb9
Annika Ehlers <e_hlers@live.dk>
2020-05-21 15:26:19 +0200

    Finish web scraper

--- scraper.py
+++ scraper.py
@@ -1,42 +1,48 @@
 import bs4
 import requests
 import re
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.keys import Keys
 
 
-class Scraper:
+def search(songname, artist):
+    url = "https://www.genius.com"
+    browser = webdriver.Firefox()
+    browser.get(url)
+    browser.implicitly_wait(3)
+    query = songname + " " + artist
 
-    def __init__(self):
-        self.formattedSongName = None
+    search_field = browser.find_element_by_name("q")
+    search_field.clear()
+    search_field.send_keys(query + Keys.RETURN)
+    browser.implicitly_wait(5)
+    a = browser.find_elements_by_class_name("mini_card-thumbnail")
+    print(a)
+    a[0].click()
 
-    # previous method.
-    '''def SetSearch(self, songname, artist):
-        self.formattedSongName = (artist + "%20" + songname).replace(" ", "%20").lower()
-        return self.formattedSongName'''
+    # link = browser.find_element_by_css_selector("[href^=#about]")
+    # link = browser.find_element_by_xpath('//a[@href="#about"]')
+    # print("--------------------------", link)
+    # browser.implicitly_wait(3)
+    # link.click()
+    browser.implicitly_wait(3)
 
-    # previous method.
-    '''def GetSearchLinks(self):
-        response = requests.get("https://genius.com/search?q=" + self.formattedSongName)
-        soup = bs4.BeautifulSoup(response.content, "html.parser")
+    response = requests.get(browser.current_url)
+    print(response.status_code)
+    soup = bs4.BeautifulSoup(response.content, "html.parser")
+    comment = soup.find("div", {"class": "ikywhQ"})  # about box
+    result = (
+        "We couldn't find much buzz about this song :("
+        if comment is None
+        else re.sub(r"^https?:\/\/.*[\r\n]*", "", comment.getText(), flags=re.MULTILINE)
+    )
 
-        links = soup.find_all('a')
+    # print(result)
 
-        return links'''
+    arr = result.split("\n")
+    print(arr[0])
+    return arr[0]
 
-    def SetSearch(self, songname, artist):
-        self.formattedSongName = (
-            artist + "-" + songname + "-lyrics").replace(" ", "-").lower()
-        return self.formattedSongName
 
-    def GetTopComment(self):
-        response = requests.get("https://genius.com/" + self.formattedSongName)
-        soup = bs4.BeautifulSoup(response.content, "html.parser")
-
-        #stuff = soup.find_all('p')
-        stuff = soup.find_all("div", {"class": "hwxsoX"})
-
-        print(stuff)
-
-        #r = re.compile('<div>(.+?)</div>')
-        # print(r.findall(response.text))
-
-        return soup
+search("blank space", "taylor swift")
