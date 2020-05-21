53b88ccc889cf1790705ad3b850cb68f23846f25
weetardeded <autismico@gmail.com>
2020-05-21 12:20:47 +0200

    scraper

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # PythonExam Song Suggestions Project
-import pandas as pd
-import matplotlib.pyplot as plt
-from kmeans_helper import KMeansHelper
+#import pandas as pd
+#import matplotlib.pyplot as plt
+#from kmeans_helper import KMeansHelper
+import scraper as scraper
 
 # df = pd.read_csv("data/song_data.csv", sep=",", header=0)
 
@@ -9,7 +10,16 @@ from kmeans_helper import KMeansHelper
 
 # print(df['audio_valence'])
 
-helper = KMeansHelper(pathtofile="data/song_data.csv", clusters=300)
+#helper = KMeansHelper(pathtofile="data/song_data.csv", clusters=300)
 
-print(helper.getClusters())
-print(helper.getLabels())
+#print(helper.getClusters())
+#print(helper.getLabels())
+
+s = scraper.Scraper()
+
+s.SetSearch("Without me", "Eminem")
+#links = s.GetSearchLinks()
+
+print(s.formattedSongName)
+s.GetTopComment()
+#print(links)
---
+++ scraper.py
@@ -0,0 +1,40 @@
+import bs4
+import requests
+import re
+
+class Scraper:
+
+    def __init__(self):
+        self.formattedSongName = None
+
+    # previous method.
+    '''def SetSearch(self, songname, artist):
+        self.formattedSongName = (artist + "%20" + songname).replace(" ", "%20").lower()
+        return self.formattedSongName'''
+
+    # previous method.
+    '''def GetSearchLinks(self):
+        response = requests.get("https://genius.com/search?q=" + self.formattedSongName)
+        soup = bs4.BeautifulSoup(response.content, "html.parser")
+
+        links = soup.find_all('a')
+
+        return links'''
+
+    def SetSearch(self, songname, artist):
+        self.formattedSongName = (artist + "-" + songname + "-lyrics").replace(" ", "-").lower()
+        return self.formattedSongName
+
+
+    def GetTopComment(self):
+        response = requests.get("https://genius.com/" + self.formattedSongName)
+        soup = bs4.BeautifulSoup(response.content, "html.parser")
+
+        stuff = soup.find_all('p')
+
+        print(stuff)
+
+        #r = re.compile('<div>(.+?)</div>')
+        #print(r.findall(response.text))
+
+        return soup
